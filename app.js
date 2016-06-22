e5d4168c6de846c6bce106a8de47513ce0a67701
Cade Nichols <cade@cadenichols.com>
2016-06-22 10:20:59 -0700

    fixes

--- app.js
+++ app.js
@@ -14,14 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.set('view engine', 'pug');
-
-app.use('/posts', require('./routes/posts'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-
   res.render('index');
-
 });
 
 app.listen(PORT, err => {
